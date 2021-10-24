23106ab761c24f8228a068b26539312d361f2ab5
Josh Harding <josh.terance@gmail.com>
2021-10-24 17:18:39 -0400

    Delete db.ts

--- src/server/config/db.ts
+++
@@ -1,8 +0,0 @@
-/* db.ts
-Student Name: Joshua Harding
-Student ID: 301186067
-Date: dd/mm/yyyy = 20/10/2021 */
-export const LocalURI = "mongodb://localhost:27017/";
-export const RemoteURI = "";
-export const Secret = "123whatever456";
-export const HostName = "localhost";
